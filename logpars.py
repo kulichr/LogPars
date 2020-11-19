cf59f34e2369b5c6297217b013857885a4a900ad
Roman Kulich <rpkulich@gmail.com>
2020-11-19 23:30:16 +0100

    Create logpars.py

---
+++ logpars.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# Name: LogPars
+# Author: Roman Kulich @ 2020
+# Version: v1.0.0
+
+import argparse
+
+TGREEN =  '\033[32m'
+TWHITE = '\033[37m'
+TRED = '\033[31m'
+print(TGREEN + '''
+
+.-.    .----.  .---. .----.  .--.  .----.  .----.
+| |   /  {}  \/   __}| {}  }/ {} \ | {}  }{ {__  
+| `--.\      /\  {_ }| .--'/  /\  \| .-. \.-._} }
+`----' `----'  `---' `-'   `-'  `-'`-' `-'`----' 
+
+''',TWHITE)
+print(TGREEN + 'Usage: logpars.py "error message you are looking for" /var/log/TARGETLOG',TWHITE)
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument(dest='msg', help="logpars.py [error message] [target log]")
+parser.add_argument(dest='log', help="logpars.py [error message] [target log]")
+args = parser.parse_args()
+message = (args.msg)
+log = (args.log)
+f = open(log)
+lines = f.readlines()
+
+print("")
+print("<=== Results ===> ")
+for line in lines:
+    if line.__contains__(message):
+        print("Found: " + TRED + (line.strip()),TWHITE)
